77e8e3c65c3b1b905df3cb9a72c8acbb1e412b34
𝙑𝞊𝞰𝞸𝙢 <vemop06@gmail.com>
2024-12-24 15:08:28 +0530

    Update queue.py

--- VIPMUSIC/utils/inline/queue.py
+++ VIPMUSIC/utils/inline/queue.py
@@ -81,7 +81,7 @@ def aq_markup(_, chat_id):
         [
             InlineKeyboardButton(
                 text="๏ ᴍᴏʀᴇ ๏",
-                url="https://t.me/Nobita_Support",
+                url="https://t.me/FRIENSHIP_CLUB_GROUP",
             ),
         ],
     ]
